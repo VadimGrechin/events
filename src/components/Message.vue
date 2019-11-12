cab6c9d4a53f5a60ecb452c5aa0dfb92de35b0fb
Vadym Grechyn <vadim.grechin.1@gmail.com>
2019-11-12 18:08:28 +0200

    Исправлено: ссобщение идет как html

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -3,7 +3,7 @@
 		<v-layout align-center justify-end column fill-height>
 			<v-flex xs-12>
 				<h2 class="text-xs-center">{{titlemessage}}</h2>
-				<div class="text-xs-center">{{message}}</div>
+				<div class="text-xs-center" v-html="message"></div>
 			</v-flex>
 		</v-layout>
 	</v-container>
