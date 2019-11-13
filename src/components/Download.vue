e9091498eafa577e73be694fe8403322fc8c684a
Vadym Grechyn <vadim.grechin.1@gmail.com>
2019-11-13 11:24:59 +0200

    Промежуточный

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -1,10 +1,30 @@
 <template>
-	
+	<v-content>
+		<a v-if="link" :href="link"></a>
+		<message v-if="messageTitle" :titlemessage="messageTitle" :message="messageContent"></message>
+	</v-content>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import Message from './Message'
 
+export default {
+	name: '',
+	data() {
+		return {
+			link: '',
+			messageTitle: '',
+			messageContent: ''
+		}
+	},
+	created() {
+		// получить параметры строки запроса
+		
+	},
+	metods: {
+
+	}
 }
 </script>
 
